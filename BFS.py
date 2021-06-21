77accb837b08fd9541add91b4b4d53367e0ab680
Jonny <54864070+JonnySC@users.noreply.github.com>
2021-06-20 20:38:49 -0500

    Update BFS.py

--- BFS.py
+++ BFS.py
@@ -1,1 +1,26 @@
-def BFS(self, s):
+class Bfs:
+    g = Grafica()
+    def __ini__(self):
+        self.bfs = {}
+
+    def bfs(self, r):
+        g = Grafica()
+        if r in g.vertices:
+            cola = [r]
+
+            g.vertices[r].visitado = True
+            g.vertices[r].nivel = 0
+
+            print("("+ str(r) + ", "+ str(g.vertices[r].nivel)+")")
+
+            while (len(cola) > 0):
+                act = cola[0]
+                cola = cola[1:]
+
+                for v in g.vertices[act].vecinos:
+                    if g.vertices[v].visitado == False:
+                        cola.append(v)
+                        g.vertices[v].visitado = True
+                        g.vertices[v].nivel = g.vertices[act].nivel + 1
+                        print("(" + str (v) + ", " + str(g.vertices[v].nivel) + ")")
+
