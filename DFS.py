b38ae248e72dcced1f0aa5d0f953f1b577ee586d
Jonny <54864070+JonnySC@users.noreply.github.com>
2021-06-20 20:39:10 -0500

    Update DFS.py

--- DFS.py
+++ DFS.py
@@ -1,2 +1,14 @@
-def DFS_R(self, s):
-def DFS_I(self, s):
+class Dfs:
+    g = Grafica()
+    def __ini__(self):
+        self.dfs = {}
+
+    def dfs(self, r):
+        g = Grafica()
+        if r in g.vertices:
+            g.vertices[r].visitado = True
+            for nodo in g.vertices[r].vecinos:
+                if g.vertices[nodo].visitado == False:
+                    g.vertices[nodo].padre = r
+                    print("(" + str(nodo) + ", " + str(r) + ")")
+                    g.dfs(nodo)
